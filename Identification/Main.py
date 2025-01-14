b8747d366854631446bec78eec1484b325a2fed9
JHoffmann12 <Jakesamuelshoffmann@gmail.com>
2025-01-13 20:04:54 -0500

    stuff

--- Identification/Main.py
+++ Identification/Main.py
@@ -21,14 +21,14 @@ if __name__ == "__main__":
     base_dir = r"C:\Users\HP\Documents\JHU_Academics\Research\FilPHANGS"
     csv_path = r"C:\Users\HP\Documents\JHU_Academics\Research\FilPHANGS\ImageData.xlsx"
     param_file_path = r"C:\Users\HP\Documents\JHU_Academics\Research\FilPHANGS\SoaxParams.txt"
-    probability_threshold = .1
+    probability_threshold = .01
     min_area_pix = 75
 
-    #Create Directory
-    mainFuncs.create_directory_structure(base_dir)
-    #Clear Directory
-    # mainFuncs.clear_all_files()
+    mainFuncs.clear_all_files(base_dir, csv_path, param_file_path)
     mainFuncs.rename_fits_files(base_dir, csv_path)
+    #Create Directory
+    mainFuncs.create_directory_structure(base_dir, csv_path)
+    #Clear Directory
     #****star sub TBD****
 
     #Loop through Each Galaxy
@@ -36,23 +36,23 @@ if __name__ == "__main__":
         galaxy_folder_path = os.path.join(base_dir, Galaxy)
         if not os.path.isdir(galaxy_folder_path):  # Skip if it's not a directory
             continue
-        if(Galaxy == 'OriginalMiriImages' and Galaxy != "Figures"): #remove later
+        if(Galaxy != 'OriginalMiriImages' and Galaxy != "Figures"): #remove later
             print(f"Preparing analysis for {Galaxy}")
-            distance_Mpc,res, pixscale = mainFuncs.getInfo(Galaxy, csv_path) #get relevant information for image
+            distance_Mpc,res, pixscale, min_power, max_power = mainFuncs.getInfo(Galaxy, csv_path) #get relevant information for image
             if(not mainFuncs.decomposition_exists(galaxy_folder_path)): #decomposie original image into scales
-                Modified_Constrained_Diffusion.decompose(base_dir, Galaxy, distance_Mpc, res, pixscale)
+                Modified_Constrained_Diffusion.decompose(base_dir, Galaxy, distance_Mpc, res, pixscale, min_power, max_power)
             FilamentMapList = mainFuncs.setUpGalaxy(base_dir, galaxy_folder_path, Galaxy, distance_Mpc, res, pixscale, param_file_path) #prepare all CDD images for one galaxy at a time
-            mainFuncs.CreateSNRPlot(FilamentMapList, base_dir, Write = False, verbose = False)
+            # mainFuncs.CreateSNRPlot(FilamentMapList, base_dir, Write = False, verbose = False)
             for myFilMap in FilamentMapList: #iterate through each decomposed image
-                myFilMap.ScaleBkgSub(WriteFits = False)
+                myFilMap.ScaleBkgSub(WriteFits = True)
                 myFilMap.getNoiseLevelsHistogram(min = 10**-2, verbose = False, WriteFig = True)
                 myFilMap.RunSoaxThreads()
                 myFilMap.CreateComposite(param_file_path) #either this or run soax update to take param path
                 myFilMap.BlurComposite(set_blur_as_prob = True, WriteFits = True)
                 myFilMap.CreateProbIntensityPlot(base_dir, Orig = False, WriteFig = True, verbose = False) #setIntensityMap in this function??***********
-                myFilMap.ReHashComposite(ProbabilityThreshPercentile = .1, minPixBoxSize = 75, setAsComposite=True, WriteFits = True) 
+                myFilMap.ReHashComposite(ProbabilityThreshPercentile = probability_threshold, minPixBoxSize = min_area_pix, setAsComposite=True, WriteFits = True) 
                 myFilMap.generateSyntheticFilaments(verbose = False, WriteFits = True)
-                myFilMap.getFilamentLengthHistogram(probability_threshold = .1, verbose = False, WriteFig = True)
+                myFilMap.getFilamentLengthHistogram(probability_threshold = probability_threshold, verbose = False, WriteFig = True)
 
     #Time information
     end = time.time()
